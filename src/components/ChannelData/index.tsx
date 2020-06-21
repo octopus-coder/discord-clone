773ff0518cbd376f89f6879a6839ca3f11b400f9
Luis Enrique Gomez-Sanchez <luisen.goscas@gmail.com>
2020-06-21 00:57:20 -0300

    Discord Clone | Rocketseat

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,59 @@
+import React, {useRef, useEffect} from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        <ChannelMessage
+          author="Luis Enrique"
+          date="11/08/2020"
+          content="Hoje e meu aniversario"
+        />
+        {
+          Array.from(Array(15).keys()).map((n) => (
+            <ChannelMessage
+              key={n}
+              author="Luis Enrique"
+              date="11/08/2020"
+              content="Hoje e meu aniversario"
+            />
+          ))
+        }
+
+        <ChannelMessage
+          author="Luisen"
+          isBot
+          date="11/08/2020"
+          content={
+            <>
+              <Mention>@Luis Enrique</Mention> parabens cara.
+            </>
+          }
+        />
+
+      </Messages >
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversar em #chat-livre"/>
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
